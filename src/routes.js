035ae59a19639086ad4947aac21260ac7317e56a
ColinRoye <croye97@gmail.com>
2019-11-15 23:28:08 -0500

    minor edit to routes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ const app = express();
 const router = require("express").Router();
 const debug = require("./debug");
 const env = require("./env");
-const auth = require("./auth");
 const service = require("./services");
 
 
